6676292ec561540a7f6bcd419e3f0092456acb4d
Vitor Lescura <50837031+VitorLescura@users.noreply.github.com>
2022-03-08 11:29:32 -0300

    Update Cliente.java

--- Strategy/Código/Cliente.java
+++ Strategy/Código/Cliente.java
@@ -1,1 +1,15 @@
+package strategypattern;
 
+public class Cliente {
+
+	private Emprestimo emprestimo; //aqui o polimorfismo
+	
+	public void setEmprestimo(Emprestimo emprestimo){//argumento polimorfico
+		this.emprestimo = emprestimo;
+	}
+	
+	public void realizarEmprestimo() {
+		this.emprestimo.emprestar();
+	}
+	
+}
