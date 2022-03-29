f45622511dc3fe82e7117eb727beecddccfeed4b
Vitor Lescura <50837031+VitorLescura@users.noreply.github.com>
2022-03-29 11:16:33 -0300

    Create Teste.java

---
+++ Observer/Teste.java
@@ -0,0 +1,9 @@
+public class test{
+  
+  public static void main(String[] args){
+    objetoSimples objetoComum = new objetoComum();
+    objetoSimples objetoSimples = new objetoComum(objetoComum);
+    
+    objetoComum.setValor(80);
+  }
+}
